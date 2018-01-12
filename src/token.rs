7d0bbb3f0d487a6a267773e87ee8cffeaaca3a93
Agoston Szepessy <agszepp@gmail.com>
2018-01-12 15:16:22 -0500

    Add Token enum
    Token contains all the possible tokens that can be created when the
    lexer goes through the source.

---
+++ src/token.rs
@@ -0,0 +1,50 @@
+pub enum Token {
+    // Single character tokens
+    Comma,
+    Dot,
+    Colon,
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    LeftBrace,
+    RightBrace,
+    Plus,
+    Minus,
+    Slash,
+    Star,
+    Semicolon,
+
+    // One or two character tokens
+    Bang,
+    BangEq,
+    Eq,
+    EqEq,
+    Greater,
+    GreaterEq,
+    Less,
+    LessEq,
+
+    // Literals
+    Int(i64),
+    Float(f64),
+    String(String),
+    Identifier(String),
+
+    // Keywords
+    And,
+    Class,
+    If,
+    Else,
+    False,
+    For,
+    Fn,
+    Null,
+    Super,
+    Ret,
+    This,
+    True,
+    Val,
+    Var,
+    While,
+}
