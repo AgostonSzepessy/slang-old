9ab09b344aa0f3ad3029f7081a0be39217ad4997
Agoston Szepessy <agoston.the.dev@gmail.com>
2018-06-16 23:43:49 -0700

    Refactor binary arithmetic and add bool evaluation
    Arithmetic is implemented through traits and a macro is used for
    evaluating boolean expressions.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,18 +1,44 @@
 use parser::Expr;
 use token::Token;
 
+use std::ops::{Add, Div, Mul, Sub};
+
 #[derive(Debug, PartialEq, Fail)]
 enum InterpreterError {
     #[fail(display = "Type error")]
     TypeError,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, PartialOrd)]
 enum ExprVal {
     Num(f64),
     Bool(bool),
     String(String),
 }
+
+/// Macro that implements basic arithmetic operations
+/// for ExprVal. It returns a Result<ExprVal, InterpreterError>
+/// based on whether the types match or not.
+macro_rules! impl_arithmetic {
+    ($trait: ident, $fn: ident, $opr: tt) => {
+        impl $trait for ExprVal {
+            type Output = Result<ExprVal, InterpreterError>;
+
+            fn $fn(self, other: ExprVal) -> Result<ExprVal, InterpreterError> {
+                match (self, other) {
+                    (ExprVal::Num(n1), ExprVal::Num(n2)) => Ok(ExprVal::Num(n1 $opr n2)),
+                    _ => Err(InterpreterError::TypeError),
+                }
+            }
+        }
+    }
+}
+
+impl_arithmetic!(Add, add, +);
+impl_arithmetic!(Mul, mul, *);
+impl_arithmetic!(Sub, sub, -);
+impl_arithmetic!(Div, div, /);
+
 
 impl ExprVal {
     fn is_bool(&self) -> bool {
@@ -90,24 +116,29 @@ impl Interpreter {
                 let left_val = self.interpret(*left_side)?;
                 let right_val = self.interpret(*right_side)?;
 
-                macro_rules! bin_op {
-                    ($left: expr, $opr: tt, $right: expr, $expr_val: expr) => {{
+                /// Macro that generates a match statement for boolean expressions.
+                macro_rules! bool_impl {
+                    ($left: expr, $opr: tt, $right: expr) => {{
                         match ($left, $right) {
-                            (ExprVal::Num(n1), ExprVal::Num(n2)) => {
-                                return Ok($expr_val(n1 $opr n2));
-                            },
-                            _ => return Err(InterpreterError::TypeError)
+                            (ExprVal::Bool(b1), ExprVal::Bool(b2)) => return Ok(ExprVal::Bool(b1 $opr b2)),
+                            (ExprVal::Num(n1), ExprVal::Num(n2)) => return Ok(ExprVal::Bool(n1 $opr n2)),
+                            (ExprVal::String(s1), ExprVal::String(s2)) => return Ok(ExprVal::Bool(s1 $opr s2)),
+                            _ => return Err(InterpreterError::TypeError),
                         }
                     }}
                 }
 
                 match operator {
-                    Token::Plus(..) => bin_op!(left_val, +, right_val, ExprVal::Num),
-                    Token::Minus(..) => bin_op!(left_val, -, right_val, ExprVal::Num),
-                    Token::Star(..) => bin_op!(left_val, *, right_val, ExprVal::Num),
-                    Token::Slash(..) => bin_op!(left_val, /, right_val, ExprVal::Num),
-                    Token::Percent(..) => bin_op!(left_val, %, right_val, ExprVal::Num),
-                    Token::EqEq(..) => bin_op!(left_val, ==, right_val, ExprVal::Bool),
+                    Token::Plus(..) => left_val + right_val,
+                    Token::Minus(..) => left_val - right_val,
+                    Token::Star(..) => left_val * right_val,
+                    Token::Slash(..) => left_val / right_val,
+                    Token::EqEq(..) => bool_impl!(left_val, ==, right_val),
+                    Token::BangEq(..) => bool_impl!(left_val, !=, right_val),
+                    Token::Greater(..) => bool_impl!(left_val, >, right_val),
+                    Token::GreaterEq(..) => bool_impl!(left_val, >=, right_val),
+                    Token::Less(..) => bool_impl!(left_val, <, right_val),
+                    Token::LessEq(..) => bool_impl!(left_val, <=, right_val),
 
                     _ => panic!(),
                 }
@@ -148,6 +179,19 @@ mod tests {
     gen_test!(test_true_primary, Expr::Primary(Token::True(1, 1)), ExprVal::Bool(true));
     gen_test!(test_false_primary, Expr::Primary(Token::False(1, 1)), ExprVal::Bool(false));
     gen_test!(test_string_primary, Expr::Primary(Token::String("slang".to_string(), 1, 1)), ExprVal::String("slang".to_string()));
+
+    macro_rules! gen_binary_test {
+        ($name: ident, $opr: expr, $expect: expr) => {
+            #[test]
+            fn $name() {
+                let interpreter = Interpreter::new();
+                let result = interpreter.interpret(Expr::Binary(Box::new(Expr::Primary(Token::Num(1f64, 0, 0))), $opr, Expr::Binary(Box::new(Expr::Primary(Token::Num(1f64, 0, 0))))));
+
+                assert_eq!(result, ExprVal::Num($expect));
+            }
+        }
+    }
     
     gen_test!(test_add_binary, Expr::Binary(Box::new(Expr::Primary(Token::Num(1f64, 0, 0))), Token::Plus(0, 0), Box::new(Expr::Primary(Token::Num(1f64, 0, 0)))), ExprVal::Num(2f64));
+    gen_binary_test!(test_eqeq, Token::EqEq(0, 0), 1f64);
 }
